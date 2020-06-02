b5f517c8761bc301b7a748160a339755f4bc2c1b
Alexgh31 <66224184+Alexgh31@users.noreply.github.com>
2020-06-01 22:38:11 -0500

    Create Lab4.js

---
+++ Lab4.js
@@ -0,0 +1,14 @@
+// primer problema
+
+var a = 3;
+var b = 4;
+console.log(`La suma da el resultado de ${a + b}`);
+console.log(`La resta da el resultado de ${a - b}`);
+console.log(`la multiplicación da el resultado de ${a * b}`);
+console.log(`la division da el resultado de ${a / b}`);
+
+// segundo problema
+
+let n = "Alexander";
+let l = "Hawkins";
+console.log(`${n} ${l}`);
